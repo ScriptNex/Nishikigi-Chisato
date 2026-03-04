9ad0547c736e4e0b048282d6efd278e0566ec368
Neykoor💜 <chinquepapa@gmail.com>
2026-03-03 18:52:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 import { createAPI } from "whispa-js"
 import config from "./config.js"
 
-async function start(){
+async function start() {
 
- try{
+ await createAPI(config)
 
-  console.log(`🌸 Iniciando ${config.botName}...`)
-
-  await createAPI(config)
-
- }catch(e){
-
-  console.error("❌ Error iniciando bot")
-
-  console.error(e)
-
-  process.exit(1)
-
- }
+ console.log("🌸 Nishikigi Chisato iniciada")
 
 }
 
