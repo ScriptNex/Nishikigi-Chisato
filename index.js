f82058b730b839bafe00201791d670bedc84d63f
Neykoor💜 <chinquepapa@gmail.com>
2026-03-03 22:23:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 import { createAPI } from "whispa-js"
+import { createConnection } from "whispa-js/src/core/connection.js"
 import config from "./config.js"
 
 async function start() {
 
- await createAPI(config)
+ const connection = await createConnection({
+  sessionPath: config.sessionPath,
+  botName: config.botName,
+  logger: console
+ })
+
+ const api = await createAPI({
+  ...config,
+  connection,
+  logger: console
+ })
+
+ api.start()
+
+ connection.sock.ev.on("messages.upsert", async ({ messages }) => {
+
+  const msg = messages?.[0]
+  if (!msg || !msg.message) return
+
+  await api.handle({
+   raw: msg,
+   sock: connection.sock
+  })
+
+ })
 
  console.log("🌸 Nishikigi Chisato iniciada")
 
