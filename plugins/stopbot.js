465f8bfa5ec8f6cd8fa6fc60de246605fe74c34d
Neykoor💜 <chinquepapa@gmail.com>
2026-03-04 08:32:04 -0800

    Update stopbot.js

--- plugins/stopbot.js
+++ plugins/stopbot.js
@@ -1,10 +1,17 @@
 export default {
 
+ name: "stopbot",
  command: "stopbot",
+ owner: true,
 
  async execute(ctx) {
 
   const id = ctx.sender.split("@")[0]
+
+  if (!ctx.sock?.stopSubBot) {
+   await ctx.reply("⚠️ Sistema de subbots no disponible")
+   return
+  }
 
   ctx.sock.stopSubBot(id)
 
