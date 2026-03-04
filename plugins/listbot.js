d104b1ebb4d8f906cb48a90c97271257e3a0c66d
Neykoor💜 <chinquepapa@gmail.com>
2026-03-03 18:49:24 -0800

    Update listbot.js

--- plugins/listbot.js
+++ plugins/listbot.js
@@ -7,13 +7,13 @@ export default {
   const bots = ctx.sock.listSubBots()
 
   if (!bots.length) {
-    return ctx.reply("No hay subbots activos")
+   return ctx.reply("🤖 No hay subbots activos")
   }
 
-  let text = "🤖 Subbots activos:\n\n"
+  let text = "🤖 *Subbots activos*\n\n"
 
   bots.forEach((id,i)=>{
-    text += `${i+1}. ${id}\n`
+   text += `${i+1}. ${id}\n`
   })
 
   await ctx.reply(text)
