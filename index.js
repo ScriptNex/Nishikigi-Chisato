b6a8555ab73f4645602c559ba4398b0b5f38d4ab
Neykoor💜 <chinquepapa@gmail.com>
2026-03-04 10:57:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,29 +5,37 @@ async function start() {
 
  const connection = await createConnection({
   sessionPath: config.sessionPath,
-  botName: config.botName
+  botName: config.botName,
+  logger: console
  })
 
  const api = await createAPI({
-  ...config,
-  connection
+  prefix: config.prefix,
+  pluginsDir: config.pluginsDir,
+  connection,
+  logger: console
  })
 
- connection.sock.ev.on("messages.upsert", async ({ messages }) => {
+ connection.onSocketUpdate(sock => {
 
-  const msg = messages?.[0]
-  if (!msg?.message) return
+  sock.ev.on("messages.upsert", async ({ messages }) => {
 
-  await api.handle({
-   raw: msg,
-   sock: connection.sock
+   const msg = messages?.[0]
+
+   if (!msg?.message) return
+
+   await api.handle({
+    raw: msg,
+    sock
+   })
+
   })
 
  })
 
  api.start()
 
- console.log("🌸 Nishikigi Chisato iniciada")
+ console.log("🌸 Nishikigi Chisato iniciado")
 
 }
 
