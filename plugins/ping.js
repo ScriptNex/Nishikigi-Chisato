e98647eb70afc5c05f91fcaa30789bbf105ce11e
Neykoor💜 <chinquepapa@gmail.com>
2026-03-04 06:14:32 -0800

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,20 +1,9 @@
 export default {
 
  command: "ping",
- aliases: ["test"],
 
  async execute(ctx) {
-
-  const start = Date.now()
-
-  await ctx.react("🏓")
-
-  const end = Date.now()
-
-  const speed = end - start
-
-  await ctx.reply(`🏓 Pong\n⚡ ${speed} ms`)
-
+  await ctx.reply("pong")
  }
 
 }
