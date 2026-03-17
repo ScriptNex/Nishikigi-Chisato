70d3e07581e2e2490bcd383b9040f83089e70d07
Neykoor💜 <chinquepapa@gmail.com>
2026-03-16 21:22:49 -0700

    Update PluginLoader.ts

--- src/core/PluginLoader.ts
+++ src/core/PluginLoader.ts
@@ -1,7 +1,29 @@
+import fs from 'fs'
+import path from 'path'
+
 export class PluginLoader {
-    constructor() {}
+    commands: Map<string, any>
+
+    constructor() {
+        this.commands = new Map()
+    }
 
     async loadCommands(dir: string) {
-        return { commandMap: new Map(), beforeHandlers: [] };
+        const files = fs.readdirSync(dir, { recursive: true })
+
+        for (const file of files) {
+            if (!file.toString().endsWith('.ts')) continue
+
+            const fullPath = path.join(dir, file.toString())
+            const module = await import(fullPath)
+
+            const cmd = module.default
+
+            if (cmd?.name) {
+                this.commands.set(cmd.name, cmd)
+            }
+        }
+
+        return this.commands
     }
 }
