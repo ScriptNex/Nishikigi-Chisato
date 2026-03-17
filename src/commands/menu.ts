5dd2b6c8a7b21c3539df9ee612cf527f5667997c
Neykoor💜 <chinquepapa@gmail.com>
2026-03-16 21:25:58 -0700

    Create menu.ts
    Beta

---
+++ src/commands/menu.ts
@@ -0,0 +1,19 @@
+export default {
+    name: 'menu',
+
+    async execute({ bot, message }: any) {
+        const chat = message.key.remoteJid
+
+        const text = `
+╭━━━〔 Nishikigi Chisato 〕━━━⬣
+┃
+┃ 💴 ECONOMÍA
+┃ ├ #balance
+┃ ├ #work
+┃
+╰━━━━━━━━━━━━━━━━⬣
+        `.trim()
+
+        await bot.sendMessage(chat, { text })
+    }
+}
