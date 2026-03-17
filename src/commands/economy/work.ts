c230e4fd4f244b339f8a3a225228fbe283ae63b7
Neykoor💜 <chinquepapa@gmail.com>
2026-03-16 23:06:38 -0700

    Update work.ts

--- src/commands/economy/work.ts
+++ src/commands/economy/work.ts
@@ -1,16 +1,9 @@
 export default {
     name: 'work',
-
     async execute({ bot, message, services }: any) {
         const sender = message.key.remoteJid;
-        if (!services?.economy) return;
-
         const reward = Math.floor(Math.random() * 200) + 100;
-
         services.economy.addMoney(sender, reward);
-
-        await bot.sendMessage(sender, {
-            text: `💼 Trabajaste y ganaste ¥${reward}`
-        });
+        await bot.sendMessage(sender, { text: `💼 Trabajaste y ganaste ¥${reward}` });
     }
 }
