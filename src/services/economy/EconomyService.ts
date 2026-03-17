3fbfd36d02192c6e6ab723e64b4b53de18ae62cd
Neykoor💜 <chinquepapa@gmail.com>
2026-03-16 22:56:19 -0700

    Update EconomyService.ts

--- src/services/economy/EconomyService.ts
+++ src/services/economy/EconomyService.ts
@@ -1,36 +1,19 @@
 export class EconomyService {
-    users: Map<string, { yenes: number; bank: number }>
+    users: Map<string, { yenes: number; bank: number }>;
 
     constructor() {
-        this.users = new Map()
+        this.users = new Map();
     }
 
     getUser(userId: string) {
         if (!this.users.has(userId)) {
-            this.users.set(userId, { yenes: 0, bank: 0 })
+            this.users.set(userId, { yenes: 0, bank: 0 });
         }
-        return this.users.get(userId)!
+        return this.users.get(userId)!;
     }
 
     addMoney(userId: string, amount: number) {
-        const user = this.getUser(userId)
-        user.yenes += amount
-        this.users.set(userId, user)
-    }
-
-    deposit(userId: string, amount: number) {
-        const user = this.getUser(userId)
-        if (user.yenes >= amount) {
-            user.yenes -= amount
-            user.bank += amount
-        }
-    }
-
-    withdraw(userId: string, amount: number) {
-        const user = this.getUser(userId)
-        if (user.bank >= amount) {
-            user.bank -= amount
-            user.yenes += amount
-        }
+        const user = this.getUser(userId);
+        user.yenes += amount;
     }
 }
