163c5a865186abb6f5c100e263f8cb5af59a9dba
Neykoor💜 <chinquepapa@gmail.com>
2026-03-16 23:06:57 -0700

    Update balance.ts

--- src/commands/economy/balance.ts
+++ src/commands/economy/balance.ts
@@ -1,12 +1,8 @@
 export default {
     name: 'balance',
-
     async execute({ bot, message, services }: any) {
         const sender = message.key.remoteJid;
-        if (!services?.economy) return;
-
         const user = services.economy.getUser(sender);
-
         await bot.sendMessage(sender, {
             text: `💴 Balance\n\n💰 Yenes: ¥${user.yenes}\n🏦 Banco: ¥${user.bank}`
         });
